ed7d679c0485761ffbf5cfbcaf6940c24b23e93f
Yufeng2333 <1660743717@qq.com>
2019-10-30 22:15:30 +0800

    amend

--- work8/goldbach.js
+++ work8/goldbach.js
@@ -1,22 +1,31 @@
 function goldbach(){
-    var odd = 0 ;
-    for(var i=1;i<=odd;i++){
-        if(odd % 1 == 0){
-            odd++
-        }
-    }
-    if(odd==2){
-        return true
-    }else{
+    var value = document.getElementById('odd').value
+    if(value % 2 !== 0 || value == 2){
+        console.log('非大于二的偶数')
         return false
     }
-}
 
-function isTrue(odd){
-    for(var i=2;i<odd-2;i++){
-        var j = odd-i;
-        if(ifZhishu(i)&&ifZhishu(j)){
-            console.log(odd+"可以拆分为两个质数"+i+"与"+j+"的和")
+    var goldbach = document.getElementById('goldbach')
+        var arr = []
+        var a;
+    for (var x=2;x<=value;x++ ){
+                a = 0
+        for(var y=2;y<x;y++){
+            if(x%y==0){
+                a++
+            }
+        }
+        if(a==0){
+            arr.push(x)
         }
     }
+    var str = ''
+    for (let x=0;x<arr.length/2;x++){
+        for (let y=0;y<arr.length;y++){
+            if((arr[x]+arr[y])==Number(value )){
+                str +='<div>'+value+"可以拆分为两个质数"+arr[x]+"与"+arr[y]+"的和"+'</div>'
+            }
+        }
+    }
+    goldbach.innerHTML = str
 }
