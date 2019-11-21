e5bc13aadaec57f730d17676f51586abb06b7d21
Yufeng2333 <1660743717@qq.com>
2019-11-21 11:37:00 +0800

    work10

--- work10/index.js
+++ work10/index.js
@@ -22,8 +22,18 @@ function monkey(){
     document.getElementById('monkeyking').innerText=monkey[0]
 }
 
-
 function  stat(){
     let str =document.getElementById('str').value
     let  obj ={}
+    str = str.split("").sort()
+    console.log(str)
+    for (let i = 0; i < str.length; ++i){
+        let linshi = str[i]
+        obj[linshi] = (obj[linshi] + 1) || 1;
+
+    }
+
+    document.getElementById('result').innerHTML = JSON.stringify(obj)
+
 }
+
