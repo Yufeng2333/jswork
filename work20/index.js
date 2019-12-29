2e1188d0c44a9fe324f9465209164aaae51b37a7
Yufeng2333 <1660743717@qq.com>
2019-12-29 17:23:45 +0800

    amend

--- work20/index.js
+++ work20/index.js
@@ -1,12 +1,13 @@
+//默认情况下，显示第一个分类下的菜单
 $('#fold>ul>li:first').find('.wrap').css({
-    display: 'block'
+    display:'block'
 });
-
-$('#fold>ul>li').click(function(){
+//根据用户单击，折叠或展开对应的菜单
+$('#fold>ul>li').click(function () {
     $(this).siblings('li').find('.wrap').css({
-        display: 'none'
+        display:'none'
     });
     $(this).find('.wrap').css({
-        display: 'block'
-    })
-})
+        display:'block'
+    });
+});
